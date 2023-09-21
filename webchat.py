abd19699e845612a668055024b3aa073a8407fa1
Anthony Russano <me@anthonyrussano.com>
2023-09-20 20:44:32 -0700

    Update webchat.py

--- webchat.py
+++ webchat.py
@@ -3,12 +3,21 @@ import socket
 import time
 from vars import SSID, PASSWORD
 from machine import Pin
-import urllib.parse  # For URL decoding
 
 led = Pin(15, Pin.OUT)
 
 ssid = SSID
 password = PASSWORD
+
+def url_decode(s):
+    replacements = {'%20': ' ', '%21': '!', '%22': '"', '%23': '#', '%24': '$',
+                    '%25': '%', '%26': '&', '%27': "'", '%28': '(', '%29': ')',
+                    '%2A': '*', '%2B': '+', '%2C': ',', '%2D': '-', '%2E': '.',
+                    '%2F': '/', '%3A': ':', '%3B': ';', '%3C': '<', '%3D': '=',
+                    '%3E': '>', '%3F': '?', '%40': '@', '%5B': '[', '%5D': ']'}
+    for encoded, decoded in replacements.items():
+        s = s.replace(encoded, decoded)
+    return s
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
@@ -20,11 +29,21 @@ text_file_content = "This is a shared text file. Feel free to edit!"
 # HTML template
 html_template = """<!DOCTYPE html>
 <html>
-    <head> <title>Pico Editor</title> </head>
+    <head> 
+        <title>Pico Editor</title> 
+        <script type="text/javascript">
+            function fetchUpdates(){
+                fetch('/fetch').then(response => response.text()).then(data => {
+                    document.getElementById("editor").value = data;
+                });
+            }
+            setInterval(fetchUpdates, 2000);  // Fetch updates every 2 seconds
+        </script>
+    </head>
     <body>
         <h1>Pico Collaborative Editor</h1>
         <form action="/" method="post">
-            <textarea name="text" rows="10" cols="30">{}</textarea><br>
+            <textarea id="editor" name="text" rows="10" cols="30">{}</textarea><br>
             <input type="submit" value="Update">
         </form>
     </body>
@@ -68,12 +87,17 @@ while True:
             marker = "\r\n\r\n"
             post_data_start = request.find(marker) + len(marker)
             post_data = request[post_data_start:]
-            new_text = urllib.parse.unquote_plus(post_data[5:])
+            new_text = url_decode(post_data[5:])
             text_file_content = new_text
 
-        response = html_template.format(text_file_content)
-        cl.send("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n")
-        cl.send(response)
+        if "/fetch" in request:
+            cl.send("HTTP/1.0 200 OK\r\nContent-type: text/plain\r\n\r\n")
+            cl.send(text_file_content)
+        else:
+            response = html_template.format(text_file_content)
+            cl.send("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n")
+            cl.send(response)
+
         cl.close()
 
     except OSError as e:
