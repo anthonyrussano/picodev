64e90676c97496e9efb130e003bb9f4cdc41dd0d
Anthony Russano <me@anthonyrussano.com>
2023-09-20 20:53:56 -0700

    Update webchat.py

--- webchat.py
+++ webchat.py
@@ -1,7 +1,8 @@
 import network
 import socket
 import time
-from vars import SSID, PASSWORD
+import ure
+from vars import SSID, PASSWORD  # Make sure to provide these variables in a file named vars.py
 from machine import Pin
 
 led = Pin(15, Pin.OUT)
@@ -9,51 +10,29 @@ led = Pin(15, Pin.OUT)
 ssid = SSID
 password = PASSWORD
 
-def url_decode(s):
-    replacements = {'%20': ' ', '%21': '!', '%22': '"', '%23': '#', '%24': '$',
-                    '%25': '%', '%26': '&', '%27': "'", '%28': '(', '%29': ')',
-                    '%2A': '*', '%2B': '+', '%2C': ',', '%2D': '-', '%2E': '.',
-                    '%2F': '/', '%3A': ':', '%3B': ';', '%3C': '<', '%3D': '=',
-                    '%3E': '>', '%3F': '?', '%40': '@', '%5B': '[', '%5D': ']'}
-    for encoded, decoded in replacements.items():
-        s = s.replace(encoded, decoded)
-    return s
-
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, password)
 
-# Initialize the text file
-text_file_content = "This is a shared text file. Feel free to edit!"
+messages = []
 
-# HTML template
-html_template = """<!DOCTYPE html>
+html = """<!DOCTYPE html>
 <html>
-    <head> 
-        <title>Pico Editor</title> 
-        <script type="text/javascript">
-            function fetchUpdates(){
-                fetch('/fetch')
-                    .then(response => response.text())
-                    .then(data => {
-                        document.getElementById("editor").value = data;
-                        })
-                    .catch((error) => console.error('Fetch error:', error));
-                        }
-            setInterval(fetchUpdates, 2000);  // Fetch updates every 2 seconds
-        </script>
-    </head>
-    <body>
-        <h1>Pico Collaborative Editor</h1>
+    <head> <title>Pico W</title> </head>
+    <body> 
+        <h1>Pico W</h1>
         <form action="/" method="post">
-            <textarea id="editor" name="text" rows="10" cols="30">{}</textarea><br>
-            <input type="submit" value="Update">
+            <input type="text" name="message" placeholder="Type your message">
+            <input type="submit" value="Send">
         </form>
+        <ul>
+            %s
+        </ul>
     </body>
 </html>
 """
 
-# Wait for WLAN connection
+# Wait for network connection
 max_wait = 10
 while max_wait > 0:
     if wlan.status() < 0 or wlan.status() >= 3:
@@ -77,30 +56,25 @@ s.listen(1)
 
 print("listening on", addr)
 
-# Listen for connections
 while True:
     try:
         cl, addr = s.accept()
         print("client connected from", addr)
         request = cl.recv(1024)
-        request = str(request, 'utf-8')
+        request = str(request)
 
-        if "POST" in request:
-            # Locate and decode the text
-            marker = "\r\n\r\n"
-            post_data_start = request.find(marker) + len(marker)
-            post_data = request[post_data_start:]
-            new_text = url_decode(post_data[5:])
-            text_file_content = new_text
+        # Extract message from POST data
+        match = ure.search("message=(.+?) ", request)
+        if match:
+            message = match.group(1).replace('+', ' ')
+            messages.append(message)
 
-        if "/fetch" in request:
-            cl.send("HTTP/1.0 200 OK\r\nContent-type: text/plain\r\n\r\n")
-            cl.send(text_file_content)
-        else:
-            response = html_template.format(text_file_content)
-            cl.send("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n")
-            cl.send(response)
+        # Create response
+        message_list = "".join(["<li>{}</li>".format(m) for m in messages])
+        response = html % message_list
 
+        cl.send("HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n")
+        cl.send(response)
         cl.close()
 
     except OSError as e:
